d854ae2454b8d8259070be3d05149f2bea62675e
Jeseop Hong <ds0794@naver.com>
2022-05-19 23:13:27 +0900

    :rocket: React에서 리스트 렌더링 하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+// import React, { useState, useEffect } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "홍제섭",
+    content: "아자아자",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "노영훈",
+    content: "아자아자",
+    emotion: 4,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "정성운",
+    content: "아자아자",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
