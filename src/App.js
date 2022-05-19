f86faa4cebb0c0f129c4d4074291018b2e03c105
Jeseop Hong <ds0794@naver.com>
2022-05-20 01:10:59 +0900

    :rocket: React에서 리스트 데이터 추가하기

--- src/App.js
+++ src/App.js
@@ -1,37 +1,55 @@
-// import React, { useState, useEffect } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 
-const dummyList = [
-  {
-    id: 1,
-    author: "홍제섭",
-    content: "아자아자",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "노영훈",
-    content: "아자아자",
-    emotion: 4,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "정성운",
-    content: "아자아자",
-    emotion: 3,
-    created_date: new Date().getTime(),
-  },
-];
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: "홍제섭",
+//     content: "아자아자",
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: "노영훈",
+//     content: "아자아자",
+//     emotion: 4,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: "정성운",
+//     content: "아자아자",
+//     emotion: 3,
+//     created_date: new Date().getTime(),
+//   },
+// ];
 
 function App() {
+  const [data, setData] = useState([]);
+  // 일기가 없으니 빈 배열로 출발
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
   return (
     <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={dummyList} />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 }
