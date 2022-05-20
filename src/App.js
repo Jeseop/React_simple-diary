a517b1f26e39b3b3869b5ee7b4b245fce0b6e6a2
Jeseop Hong <ds0794@naver.com>
2022-05-21 03:11:36 +0900

    :rocket: React에서 API 호출하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
+
+// https://jsonplaceholder.typicode.com/comments
 
 function App() {
   const [data, setData] = useState([]);
   // 일기가 없으니 빈 배열로 출발
 
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  //Mount 될때 실행하기 위해
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
