6fe5647cffef20bc3974dcce24244236ef60f489
Jeseop Hong <ds0794@naver.com>
2022-05-22 06:06:56 +0900

    :rocket: useMemo를 통한 연산 결과 재사용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 
-// https://jsonplaceholder.typicode.com/comments
-
-function App() {
+const App = () => {
   const [data, setData] = useState([]);
   // 일기가 없으니 빈 배열로 출발
 
@@ -62,12 +60,28 @@ function App() {
     );
   };
 
+  const getDiaryAnalysis = useMemo(() => {
+    console.log("일기 분석 시작");
+
+    const goodCount = data.filter((it) => it.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]);
+
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+  // useMemo로 함수를 최적화하면 memoizedValue는 더이상 함수가 아니게 됨으로, 함수호출이 아닌 값 호출로 사용해야함.
+
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 : {data.length}</div>
+      <div>기분 좋은 일기 개수 : {goodCount}</div>
+      <div>기분 나쁜 일기 개수 : {badCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio}</div>
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
-}
+};
 
 export default App;
